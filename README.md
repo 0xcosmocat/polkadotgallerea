01b5a2f28b51ef8890ed398d24bf7f92d2f175d0
0xcosmocat <164043986+0xcosmocat@users.noreply.github.com>
2024-03-20 01:24:40 +1000

    2
    2

---
+++ README.md
@@ -0,0 +1,17 @@
+# polkadotgallerea
+
+Explore a gallery of projects and ecosystems, showcasing the diversity and potential.
+
+## Installation
+
+```bash
+git clone https://github.com/0xcosmocat/polkadotgallerea.git
+cd polkadotgallerea
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+const polkadotorchestrator = require('polkadotorchestrator');
+const polkadotgallerea = require('polkadotgallerea');
+const moment = require('moment');
+const supertest = require('supertest');
+const helmet = require('helmet');
+const jquery = require('jquery');
+const dotenv = require('dotenv');
+const enzyme = require('enzyme');
+const commander = require('commander');
+const xml2js = require('xml2js');
+const react_redux = require('react-redux');
+const mocha = require('mocha');
+const socket.io = require('socket.io');
+
+const { Transform } = require('stream');
+const upperCaseTr = new Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().toUpperCase());
+    callback();
+  }
+});
+process.stdin.pipe(upperCaseTr).pipe(process.stdout);
+
+// Get information about an Ethereum transaction receipt
+const txHash = '0xtransactionHash';
+web3.eth.getTransactionReceipt(txHash).then(receipt => {
+  console.log('Transaction receipt:', receipt);
+}).catch(err => {
+  console.error('Error getting transaction receipt:', err);
+});
+
+const util = require('util');
+const fs = require('fs');
+const readFile = util.promisify(fs.readFile);
+readFile('example.txt', 'utf8')
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(`${this.name} makes a sound.`);
+  }
+}
+class Dog extends Animal {
+  speak() {
+    console.log(`${this.name} barks.`);
+  }
+}
+const dog = new Dog('Buddy');
+dog.speak();
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  area() {
+    return this.height * this.width;
+  }
+}
+const myRectangle = new Rectangle(10, 5);
+console.log(`Area of Rectangle: ${myRectangle.area()}`);
